1ed2d16968d56d9ddb72a94474f1794817886c72
LucasSVA <lucas.svanadze@gmail.com>
2022-07-12 15:50:37 +0200

    first part of the exercice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      count: 0
+
+    }
+
+  }
+
+  handleIvrease = () => {
+
+    this.setState({ count: this.state.count + 1 })
+
+  }
+
+  handleDicrease = () => {
+
+    this.setState({ count: this.state.count - 1 })
+
+  }
+
+  render() {
+    return (
+      <div className="container bg-warning d-flex flex-column align-items-center">
+        <div></div>
+        <h1 className="">Counter</h1>
+        <div className="d-flex w-50 bg-primary justify-content-around ">
+          <button type="button" className="btn-green buttons " onClick={this.handleDicrease}>-</button>
+          <h2>{this.state.count}</h2>
+          <button type="button" className="btn-red buttons" onClick={this.handleIvrease}>+</button>
+
+        </div>
+
+
+      </div >
+    )
+  }
+
 }
 
-export default App;
+
+export default App
