f3ddd3f055f519185693b285b187217c25c4887b
LucasSVA <lucas.svanadze@gmail.com>
2022-07-12 22:29:29 +0200

    counter stylisé

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import Counter from "./componets/Counter"
 
 
 
+
 class App extends React.Component {
   constructor() {
     super()
 
     this.state = {
-      count: 0
+      count: 0,
+      count2: 0
 
     }
 
   }
 
-  handleDivrease = () => {
-
+  handleIncrease = () => {
     this.setState({ count: this.state.count + 1 })
 
   }
@@ -24,28 +25,55 @@ class App extends React.Component {
   handleDicrease = () => {
 
     this.setState({ count: this.state.count - 1 })
+    if (this.state.count <= 1) {
+      this.setState({ count: 0 })
+
+    }
+
+  }
+
+
+  handleIncrease2 = () => {
+
+    this.setState({ count2: this.state.count2 + 1 })
+
+
+  }
+
+  handleDicrease2 = () => {
+    this.setState({ count2: this.state.count2 - 1 })
+    if (this.state.count2 <= 1) {
+      this.setState({ count2: 0 })
+
+    }
 
   }
 
   render() {
     return (
-      <div className="container bg-warning d-flex flex-column align-items-center">
-        <div></div>
+      <div className="container d-flex flex-column align-items-center vh-100 ">
         <h1 className="">Counter</h1>
-        <div className="d-flex w-50 bg-primary justify-content-around ">
+        <div className="d-flex w-50 h-75 justify-content-around flex-column mb-5 ">
           <Counter count={this.state.count}
-            increment={this.handleDivrease}
+            increment={this.handleIncrease}
             substract={this.handleDicrease}
           />
-
+          <Counter count={this.state.count2}
+            increment={this.handleIncrease2}
+            substract={this.handleDicrease2}
+          />
         </div>
-
 
       </div >
     )
+
   }
 
 }
+
+
+
+
 
 
 export default App
--- src/componets/Counter.js
+++ src/componets/Counter.js
@@ -3,11 +3,11 @@ import React from "react";
 class Counter extends React.Component {
 
     render() {
-        console.log(this.props);
+
         return (
-            <div>
+            <div className="d-flex justify-content-around align-items-center box-counter ">
                 <button type="button" className="btn-green buttons " onClick={this.props.substract}>-</button>
-                <h2>{this.props.count}</h2>
+                <h2 >{this.props.count}</h2>
                 <button type="button" className="btn-red buttons" onClick={this.props.increment}>+</button>
 
             </div>
