aa1c00c2d0bf853b126df3fe573948ccb580acd9
LucasSVA <lucas.svanadze@gmail.com>
2022-07-13 10:06:28 +0200

    styles

--- src/App.js
+++ src/App.js
@@ -57,12 +57,11 @@ class App extends React.Component {
       this.setState({ count2: 1 })
 
     }
-
     this.setState({ count: this.state.count - 1 })
 
     if (this.state.count <= 0) {
 
-      this.setState({ count: 0 })
+      this.setState({ count: this.state.count })
     }
 
   }
